89eb1a8d48de3742c2799273b15d84f9cad0eea7
Sanjay Purswani <sanjsanj@hotmail.com>
2019-05-17 19:07:35 +0100

    Add contact button

--- src/AppBar.js
+++ src/AppBar.js
@@ -1,24 +1,33 @@
 import React from "react";
 import PropTypes from "prop-types";
+
 import { withStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import Menu from "@material-ui/core/Menu";
+import MenuList from "@material-ui/core/MenuList";
 import MenuItem from "@material-ui/core/MenuItem";
 import Paper from "@material-ui/core/Paper";
-import Fab from "@material-ui/core/Fab";
+import Collapse from "@material-ui/core/Collapse";
+import ExpandMore from "@material-ui/icons/ExpandMore";
+import ExpandLess from "@material-ui/icons/ExpandLess";
+import StarBorder from "@material-ui/icons/StarBorder";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import EmailIcon from "@material-ui/icons/Email";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import ListSubheader from "@material-ui/core/ListSubheader";
 import Avatar from "@material-ui/core/Avatar";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+
 import MenuIcon from "@material-ui/icons/Menu";
-import AccountCircleIcon from "@material-ui/icons/AccountCircle";
-import AddIcon from "@material-ui/icons/Add";
-import SearchIcon from "@material-ui/icons/Search";
-import MoreIcon from "@material-ui/icons/MoreVert";
+import SendIcon from "@material-ui/icons/Send";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import InboxIcon from "@material-ui/icons/Inbox";
+import { Divider } from "@material-ui/core";
 
 const styles = theme => ({
   appBar: {
@@ -39,49 +48,76 @@ const styles = theme => ({
 function BottomAppBar(props) {
   const { classes } = props;
 
-  const [accountAnchorEl, setAccountAnchorEl] = React.useState(null);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [open, setOpen] = React.useState(false);
 
   return (
     <AppBar position="fixed" color="primary" className={classes.appBar}>
       <Toolbar className={classes.toolbar}>
         <Typography variant="h6" color="inherit" className={classes.grow}>
-          traintimes
+          material_app
         </Typography>
 
         <div>
           <IconButton
-            aria-owns={accountAnchorEl ? "menu-appbar" : undefined}
-            onClick={e => setAccountAnchorEl(e.currentTarget)}
+            aria-owns={anchorEl ? "menu-appbar" : undefined}
+            onClick={e => setAnchorEl(e.currentTarget)}
+            aria-label="Open drawer"
             aria-haspopup="true"
             color="inherit"
           >
-            <AccountCircleIcon />
+            <MenuIcon />
           </IconButton>
 
           <Menu
             transformOrigin={{
-              vertical: "center",
+              vertical: "bottom",
               horizontal: "center",
             }}
             anchorOrigin={{
               vertical: "top",
               horizontal: "center",
             }}
-            onClose={() => setAccountAnchorEl(null)}
-            anchorEl={accountAnchorEl}
-            open={!!accountAnchorEl}
+            onClose={() => setAnchorEl(null)}
+            anchorEl={anchorEl}
+            open={!!anchorEl}
             id="menu-account"
           >
-            <MenuItem disabled>Signed in as user.email</MenuItem>
+            <MenuItem>About</MenuItem>
+            <MenuItem>Cookie policy</MenuItem>
 
-            <MenuItem selected onClick={() => console.log("auth().signOut()")}>
+            <Divider />
+
+            <MenuItem>
+              <ListItemIcon>
+                <EmailIcon />
+              </ListItemIcon>
+
+              <a
+                href="mailto:hello@material_app.com"
+                rel="noopener noreferrer"
+                target="_top"
+              >
+                hello@material_app.com
+              </a>
+            </MenuItem>
+
+            <Divider />
+
+            <MenuItem disabled>
+              <ListItemIcon>
+                <AccountCircle />
+              </ListItemIcon>
+
+              <Typography variant="inherit" noWrap>
+                firstname.lastname@longemailhostname.co.uk
+              </Typography>
+            </MenuItem>
+
+            <MenuItem onClick={() => console.log("auth().signOut()")}>
               Sign out
             </MenuItem>
           </Menu>
-
-          <IconButton color="inherit" aria-label="Open drawer">
-            <MenuIcon />
-          </IconButton>
         </div>
       </Toolbar>
     </AppBar>
